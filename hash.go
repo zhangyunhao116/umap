0b61e69e958bc073f6119dc057211b63649b12d0
zhangyunhao <zhangyunhao116@gmail.com>
2025-03-07 03:13:11 +0000

    use latest mixhash

--- hash.go
+++ hash.go
@@ -2,11 +2,12 @@ package umap
 
 import "math/bits"
 
-func _wymix(a, b uint64) uint64 {
-	hi, lo := bits.Mul64(a, b)
-	return hi ^ lo
+func hashUint64(x uint64) uint64 {
+	// Mixhash: https://github.com/zhangyunhao116/mixhash
+	return _mix(x^0xcbdf2e5c79d30006, 0xfc56be937d474100)
 }
 
-func hashUint64(x uint64) uint64 {
-	return _wymix(x^0xfffc2d0600147fc8, 0xf6ea71d5ec8a2980)
+func _mix(a, b uint64) uint64 {
+	hi, lo := bits.Mul64(a, b)
+	return hi ^ lo
 }
